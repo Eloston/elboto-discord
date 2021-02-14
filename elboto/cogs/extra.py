886988463afa64ca69408bf252797ae01e4652a8
Eloston <eloston@programmer.net>
2021-02-13 17:53:49 -0800

    Add extra

---
+++ elboto/cogs/extra.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import discord.ext.typed_commands as commands
+from discord.ext.typed_commands import Cog, Context
+
+from elboto.base import Elboto
+
+
+class Extra(Cog):
+    def __init__(self, bot: Elboto):
+        self.bot = bot
+
+    @commands.command(aliases=["hi", "ping", "hey", "whatsup", "yo", "poke"])
+    async def hello(self, ctx: Context) -> None:
+        await ctx.message.add_reaction("🖖")
+
+
+def setup(bot: Elboto) -> None:
+    bot.add_cog(Extra(bot))
